6b3ccaeb8b1d9223a690b8e5feaba5aa95ddd6bd
ECHONESSY\Echonessy <545625899@qq.com>
2018-12-12 17:43:15 +0800

    合同签署，静态完成

--- controllers/index.js
+++ controllers/index.js
@@ -8,11 +8,11 @@ const crypto = require('crypto');
 exports.index = function (req, res) {
     return res.render('index');
 };
-//用户中心
+//未认证用户中心
 exports.usercer = function (req, res) {
     return res.render('user/usercer');
 };
-//用户中心
+//认证后用户中心
 exports.user = function (req, res) {
     return res.render('user/user');
 };
@@ -20,43 +20,67 @@ exports.user = function (req, res) {
 exports.wasm = function (req, res) {
     return res.render('wasm/index');
 };
+/***********************主动发起合同*************************/
 
-//合同签署
+//合同签署第一步 设置合同及签署方
+exports.appcode = function (req, res) {
+    return res.render('sign/appcode/index');
+};
+
+//合同签署第一步 设置合同及签署方
 exports.sign = function (req, res) {
-    return res.render('sign/index');
+    return res.render('sign/initiative/step1');
+};
+//合同签署第二步  拖拽签名
+exports.signnext = function (req, res) {
+    return res.render('sign/initiative/step2');
+};
+//保存草稿结果页
+exports.draftsign = function (req, res) {
+    return res.render('sign/initiative/draftsign');
+};
+//发起签署结果页
+exports.initiatesign = function (req, res) {
+    return res.render('sign/initiative/initiatesign');
+};
+//单边签署合同信息
+exports.signinfo = function (req, res) {
+    return res.render('sign/initiative/signinfo');
+};
+
+
+/***********************被动签署合同*************************/
+
+//合同拖拽生成签署合同
+exports.passivesign = function (req, res) {
+    return res.render('sign/passivity/index');
 };
 //同意签署结果
 exports.agreesign = function (req, res) {
-    return res.render('sign/agreesign');
+    return res.render('sign/passivity/agreesign');
 };
 //同意签署合同详情
 exports.agreeinfo = function (req, res) {
-    return res.render('sign/agreeinfo');
+    return res.render('sign/passivity/agreeinfo');
 };
-
 
 //拒绝签署结果
 exports.refusesign = function (req, res) {
-    return res.render('sign/refusesign');
+    return res.render('sign/passivity/refusesign');
 };
 //拒绝签署合同详情
 exports.refuseinfo = function (req, res) {
-    return res.render('sign/refuseinfo');
-};
-//保存草稿
-exports.draftsign = function (req, res) {
-    return res.render('sign/draftsign');
+    return res.render('sign/passivity/refuseinfo');
 };
 
 
-//合同签约步骤1
-exports.signstep = function (req, res) {
-    return res.render('sign/signstep');
-};
-//合同签约步骤2
-exports.signnext = function (req, res) {
-    return res.render('sign/signnext');
-};
+
+
+
+
+
+
+
 
 
 //合同管理
@@ -103,7 +127,7 @@ exports.i500 = function (req, res) {
 exports.i503 = function (req, res) {
     return res.render('error/503');
 };
-//电子签章
+//电子签章Demo
 exports.signDemo = function (req, res) {
     return res.render('demo/signDemo');
 };
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const index = require('../controllers/index');
-const wasm = require('../controllers/wasm');
+// const wasm = require('../controllers/wasm');
 router.get(/(^\/$)|^(\/index)/, index.index);
 
 //首页
@@ -10,28 +10,69 @@ router.get("/", index.index);//首页
 router.get("/usercer", index.usercer);//未认证用户中心
 router.get("/user", index.user);//用户中心
 
-router.get("/sign", index.sign);//合同签署
 
-router.get("/agreesign", index.agreesign);//同意签署结果
-router.get("/agreeinfo", index.agreeinfo);//同意签署合同详情
+/***********************合同签署*********Start****************/
 
-router.get("/refusesign", index.refusesign);//拒绝签署结果
-router.get("/refuseinfo", index.refuseinfo);//拒绝签署结果详情
+router.get("/appcode", index.appcode);
+/***********************主动发起合同*************************/
+//合同签署第一步 设置合同及签署方
+router.get("/sign", index.sign);
+router.post("/sign", index.sign);
+//合同签署第二步  拖拽签名
+router.get("/signnext", index.signnext);
+router.post("/signnext", index.signnext);
+//保存草稿结果页
+router.get("/draftsign", index.draftsign);
+router.post("/draftsign", index.draftsign);
+//发起签署结果页
+router.get("/initiatesign", index.initiatesign);
+router.post("/initiatesign", index.initiatesign);
+//单边签署合同信息
+router.get("/signinfo", index.signinfo);
+router.post("/signinfo", index.signinfo);
 
-router.get("/signstep", index.signstep);//合同签署1
-router.get("/signnext", index.signnext);//合同签署2
-router.get("/draftsign", index.draftsign);//保存草稿
+/***********************被动签署合同*************************/
+//合同拖拽生成签署合同
+router.get("/passivesign", index.passivesign);
+router.post("/passivesign", index.passivesign);
+//同意签署结果
+router.get("/agreesign", index.agreesign);
+router.post("/agreesign", index.agreesign);
+//同意签署合同详情
+router.get("/agreeinfo", index.agreeinfo);
+router.post("/agreeinfo", index.agreeinfo);
+//拒绝签署结果
+router.get("/refusesign", index.refusesign);
+router.post("/refusesign", index.refusesign);
+//拒绝签署结果详情
+router.get("/refuseinfo", index.refuseinfo);
+router.post("/refuseinfo", index.refuseinfo);
+
+
+
+/***********************合同签署*********End****************/
+
+
+
+
+
+/***********************合同管理********Start*****************/
 
 router.get("/manage", index.manage);//合同签署管理
 
+/***********************合同管理********End*****************/
+
+
+
+
+
 router.get("/wasm", index.wasm);//wasm
-
-
-
-
 //私钥解密
 router.post('/wasmKey',index.wasmKey);
+//签名demo
+router.get("/signDemo", index.signDemo);
 
+//异常
 router.get("/404", index.i404);//404
 router.get("/403", index.i403);//403
 router.get("/400", index.i400);//400
@@ -39,7 +80,5 @@ router.get("/500", index.i500);//500
 router.get("/503", index.i503);//503
 
 
-//签名demo
-router.get("/signDemo", index.signDemo);//503
 
 module.exports = router;
