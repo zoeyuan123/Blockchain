c2483f2a7c7b5772c3253d5c205fcebe4a80fad7
ECHONESSY\Echonessy <545625899@qq.com>
2018-12-27 19:54:33 +0800

    合同详细信息

--- static/js/common/common.js
+++ static/js/common/common.js
@@ -314,3 +314,21 @@ function uploadEvt(Url,formDom,fileDom,callBack) {
 
     });
 }
+
+
+function urlSearch() {
+    var name, value;
+    var str = window.location.search; //取得整个地址栏参数
+    var num = str.indexOf("?");
+    str = str.substr(num + 1); //取得所有参数   stringvar.substr(start [, length ]
+    var arr = str.split("&"); //各个参数放到数组里
+    for (var i = 0; i < arr.length; i++) {
+        num = arr[i].indexOf("=");
+        if (num > 0) {
+            name = arr[i].substring(0, num);
+            value = arr[i].substr(num + 1);
+            this[name] = value;
+        }
+    }
+}
+var getUrl = new urlSearch(); //实例化
---
+++ static/js/sign/signInfo.js
@@ -0,0 +1,75 @@
+/**
+ * Created by Echonessy on 2018/12/27.
+ */
+$(function () {
+    //获取合同信息
+    contractInfo();
+    function contractInfo() {
+        var Url = '/contractInfo';
+        var SubData = {};
+        SubData.id = getUrl.id;
+        echo.ajax.post(Url,SubData,function (res) {
+            echo.ajax.callback(res,function () {
+                console.log(res)
+                renderBaseInfo(res.data);
+            })
+        })
+    }
+    //渲染基础数据
+    function renderBaseInfo(data) {
+        $("#contractStatus").html(isExistData(data.contractStatus))
+        $("#contractName").html(isExistData(data.contractName))
+        $("#contractNo").html(isExistData(data.contractNo))
+        $("#startTime").html(isExistData(data.startTime))
+        $("#completeTime").html(isExistData(data.completeTime))
+        $("#initiatorName").html(isExistData(data.initiatorName))
+        $("#initiatorPhone").html(isExistData(data.initiatorPhone))
+        $("#contractUrl").attr('src',isExistData(data.contractUrl));
+        $("#contractStartBlockUrl").attr('href',isExistData(data.contractStartBlockUrl)).html(isExistData(data.contractStartBlockUrl));
+        $("#contractEndBlockUrl").attr('href',isExistData(data.contractEndBlockUrl)).html(isExistData(data.contractEndBlockUrl));
+        proofList(data.proofList);
+        roleList(data.roleList)
+    }
+    //鉴别数据是否存在
+    function isExistData(data) {
+        return data ? data:' ';
+    }
+    // 合同状态列表
+    function proofList(data) {
+        var Html = '';
+        Html += '<li class="single top">';
+        Html += '<span>合同状态</span>';
+        Html += '<span>文件Hash</span>';
+        Html += '</li>';
+        if(data.length > 0) {
+            for(var i=0;i<data.length;i++) {
+                var this_Data = data[i];
+                Html += '<li class="single" data-this_Data="'+JSON.stringify(this_Data)+'">';
+                Html += '<span title="'+this_Data.fileHash+'">'+this_Data.fileHash+'</span>';
+                Html += '<span title="'+this_Data.status+'">'+this_Data.status+'</span>';
+                Html += '</li>';
+            }
+        }
+        $("#proofList").html(Html);
+    }
+    //签约主体信息
+    function roleList(data) {
+        var Html = '';
+        Html += '<li class="single top">';
+        Html += '<span>签署主体</span>';
+        Html += '<span>签署方账号</span>';
+        Html += '<span>签署方公钥</span>';
+        Html += '</li>';
+        if(data.length > 0) {
+            for(var i=0;i<data.length;i++) {
+                var this_Data = data[i];
+                Html += '<li class="single" data-this_Data="'+JSON.stringify(this_Data)+'">';
+                Html += '<span title="'+this_Data.name+'">'+this_Data.name+'</span>';
+                Html += '<span title="'+this_Data.phone+'">'+this_Data.phone+'</span>';
+                Html += '<span title="'+this_Data.publicKeys+'">'+this_Data.publicKeys+'</span>';
+                Html += '</li>';
+            }
+        }
+        $("#roleList").html(Html);
+    }
+})
